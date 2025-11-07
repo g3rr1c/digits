ca2e1916f93a261390a9d19fa81f64bfb31fd05f
Gerric Abe <gerricabe@gmail.com>
2025-11-06 23:15:34 -1000

    ‘contact forms finished in 27 minutes

--- src/lib/dbActions.ts
+++ src/lib/dbActions.ts
@@ -1,6 +1,6 @@
 'use server';
 
-import { Stuff, Condition } from '@prisma/client';
+import { Stuff, Condition, Contact } from '@prisma/client';
 import { hash } from 'bcrypt';
 import { redirect } from 'next/navigation';
 import { prisma } from './prisma';
@@ -28,6 +28,52 @@ export async function addStuff(stuff: { name: string; quantity: number; owner: s
     },
   });
   // After adding, redirect to the list page
+  redirect('/list');
+}
+
+/**
+ * Adds a contact in the database.
+ * @param contact, an object with the following properties: id, name, quantity, owner, condition.
+ */
+export async function addContact(contact: {
+  firstName: string,
+  lastName: string,
+  address: string,
+  image: string,
+  description: string,
+  owner: string
+}) {
+  await prisma.contact.create({
+    data: {
+      firstName: contact.firstName,
+      lastName: contact.lastName,
+      address: contact.address,
+      image: contact.image,
+      description: contact.description,
+      owner: contact.owner,
+    },
+  });
+  redirect('/list');
+}
+
+/**
+ * Edits an existing stuff in the database.
+ * @param contact, an object with the following properties: id, name, quantity, owner, condition.
+ */
+export async function editContact(contact: Contact) {
+  // console.log(`editStuff data: ${JSON.stringify(stuff, null, 2)}`);
+  await prisma.contact.update({
+    where: { id: contact.id },
+    data: {
+      firstName: contact.firstName,
+      lastName: contact.lastName,
+      address: contact.address,
+      image: contact.image,
+      description: contact.description,
+      owner: contact.owner,
+    },
+  });
+  // After updating, redirect to the list page
   redirect('/list');
 }
 
